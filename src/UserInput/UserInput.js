1929306e6b84ad310a9e0d7a5e8238ada5ce4dee
m3c0d3 <m3c0d3@gmail.com>
2020-09-01 07:24:32 +0300

    Solution

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -4,14 +4,15 @@ class UserInput extends Component {
 
     render() {
         const style = {
-                width: '60%',
-                margin: "16px auto",
-                backgroundColor: 'white',
-                border: '1px solid blue',
-            }
-        ;
-        return <input style={style} type="text" onChange={this.props.change}
-                      defaultValue={this.props.name}/>;
+            width: '60%',
+            margin: "16px auto",
+            backgroundColor: 'white',
+            border: '1px solid blue',
+        };
+        return <p style={{textAlign: "center"}}><input style={style} type="text"
+                                                       onChange={this.props.change}
+                                                       defaultValue={this.props.name}/>;
+        </p>
     }
 }
 
